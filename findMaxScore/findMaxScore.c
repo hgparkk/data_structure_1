a6ccc355d5b56e11312acfe6db7decf4804d1c52
현규 박 <phg2559@naver.com>
2023-03-16 16:32:15 +0900

    초기 커밋.

---
+++ findMaxScore/findMaxScore.c
@@ -0,0 +1,35 @@
+﻿/*
+프로그램 내용 : 입력받은 값을 배열에 넣고 최대값을 찾는 프로그램
+실습일 : 2023.03.07
+실습자 : 박현규
+학번 : 202111678
+*/
+
+#include <stdio.h>
+
+#define MAX_ELEMENTS 5
+
+int find_max_score(int score[], int n) {
+    int i, tmp;
+    tmp = score[0];
+    for (i = 1; i < n; i++) {
+        if (score[i] > tmp) {
+            tmp = score[i];
+        }
+    }
+    return tmp;
+}
+
+int main(void)
+{
+    int score[MAX_ELEMENTS];
+    int i, res;
+
+    for (i = 0; i < MAX_ELEMENTS; i++) {
+        printf("Enter the number >>");
+        scanf("%d", &score[i]);
+    };
+    res = find_max_score(score, MAX_ELEMENTS);
+    printf("Max number = %d \n", res);
+    return 0;
+}
---
+++ findMaxScoreCheckTime/findMaxScoreCheckTime.c
@@ -0,0 +1,45 @@
+﻿/*
+프로그램 내용 : 랜덤하게 생성된 값을 배열에 넣고 최댓값을 찾는 시간을 측정하는 프로그램
+실습일 : 2023.03.07
+실습자 : 박현규
+학번 : 202111678
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_ELEMENTS 50000
+
+int find_max_score(int score[], int n) {
+    int i, tmp;
+    tmp = score[0];
+    for (i = 1; i < n; i++) {
+        if (score[i] > tmp) {
+            tmp = score[i];
+        }
+    }
+    return tmp;
+}
+
+int main(void)
+{
+    int score[MAX_ELEMENTS] = { 100,23,300 };
+    int i, res;
+    long seed;
+    clock_t start, finish;
+    double duration;
+
+    seed = time(NULL);
+    srand(seed);
+    for (i = 0; i < MAX_ELEMENTS; i++) {
+        score[i] = rand();
+    }
+    start = clock();
+    res = find_max_score(score, MAX_ELEMENTS);
+    finish = clock();
+    duration = (double)(finish - start) / CLOCKS_PER_SEC;
+    printf("%f seconds. \n", duration);
+    printf("Max number = %d \n", res);
+    return 0;
+}
